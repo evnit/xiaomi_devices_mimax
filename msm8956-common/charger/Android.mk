f1f44d891ede6294502036ec2d9204d3a6fdbe3c
Le Anh Dung <evnithn@gmail.com>
2018-05-25 10:04:44 +0700

    This is funny

--- msm8956-common/charger/Android.mk
+++ msm8956-common/charger/Android.mk
@@ -6,7 +6,7 @@ LOCAL_PATH := $(call my-dir)
 +LOCAL_MODULE_OWNER    := xiaomi
 +LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
 +LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-+LOCAL_SRC_FILES       := sbin/healthd_xiaomi
++LOCAL_SRC_FILES       := healthd_xiaomi
 +LOCAL_MODULE_TAGS     := optional
 +LOCAL_MODULE_CLASS    := EXECUTABLES
 +include $(BUILD_PREBUILT)
